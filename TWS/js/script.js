a08ca1d9a12dfa5c9326813fe311404a1b5619e2
DKM7823dc <mkyusds82a@hotmail.com>
2025-12-17 21:38:05 +0800

    Add files via upload

---
+++ TWS/js/script.js
@@ -0,0 +1,513 @@
+// 女性数据配置 - 每人配置多张图片用于slider
+const girlsData = [
+    {
+        id: 1,
+        images: ['static/images/girls/a1.jpg', 'static/images/girls/a2.jpg', 'static/images/girls/a3.jpg'],
+        name: '小雨',
+        age: 20,
+        ageGroup: '18-25',
+        style: 'sweet',
+        body: 'slim',
+        description: '甜美學生妹，喜歡探索新鮮刺激',
+        tags: ['可口交', '可內射', '可雙飛', '喜歡刺激']
+    },
+    {
+        id: 2,
+        images: ['static/images/girls/a4.jpg', 'static/images/girls/a5.jpg', 'static/images/girls/a6.jpg'],
+        name: '艾莉',
+        age: 24,
+        ageGroup: '18-25',
+        style: 'sexy',
+        body: 'curvy',
+        description: '性感OL，身材火辣豐滿誘人',
+        tags: ['可乳交', '可無套', '可後入', '技術好']
+    },
+    {
+        id: 3,
+        images: ['static/images/girls/a7.jpg', 'static/images/girls/a8.jpg', 'static/images/girls/a9.jpg'],
+        name: '婷婷',
+        age: 19,
+        ageGroup: '18-25',
+        style: 'pure',
+        body: 'slim',
+        description: '清純女大生，外表純真內心狂野',
+        tags: ['可內射', '可口交', '可顏射', '配合度高']
+    },
+    {
+        id: 4,
+        images: ['static/images/girls/a10.jpg', 'static/images/girls/a11.jpg', 'static/images/girls/a12.jpg'],
+        name: '雪兒',
+        age: 28,
+        ageGroup: '26-35',
+        style: 'mature',
+        body: 'curvy',
+        description: '成熟人妻，欲求不滿尋求刺激',
+        tags: ['可雙飛', '可無套', '可後入', '技術一流']
+    },
+    {
+        id: 5,
+        images: ['static/images/girls/a13.jpg', 'static/images/girls/a14.jpg', 'static/images/girls/a15.jpg'],
+        name: '美美',
+        age: 23,
+        ageGroup: '18-25',
+        style: 'sexy',
+        body: 'fit',
+        description: '健身教練，身材勻稱體力超好',
+        tags: ['可口交', '可乳交', '體位多', '持久力強']
+    },
+    {
+        id: 6,
+        images: ['static/images/girls/a1.jpg', 'static/images/girls/a4.jpg', 'static/images/girls/a7.jpg'],
+        name: '琳琳',
+        age: 26,
+        ageGroup: '26-35',
+        style: 'sexy',
+        body: 'slim',
+        description: '酒吧DJ，性感苗條愛玩刺激',
+        tags: ['可雙飛', '可內射', '可無套', '超會叫']
+    },
+    {
+        id: 7,
+        images: ['static/images/girls/a2.jpg', 'static/images/girls/a5.jpg', 'static/images/girls/a8.jpg'],
+        name: '安安',
+        age: 21,
+        ageGroup: '18-25',
+        style: 'sweet',
+        body: 'curvy',
+        description: '可愛護士，豐滿身材溫柔體貼',
+        tags: ['可口交', '可內射', '可乳交', '溫柔體貼']
+    },
+    {
+        id: 8,
+        images: ['static/images/girls/a3.jpg', 'static/images/girls/a6.jpg', 'static/images/girls/a9.jpg'],
+        name: '欣欣',
+        age: 32,
+        ageGroup: '26-35',
+        style: 'mature',
+        body: 'curvy',
+        description: '熟女主管，風韻猶存技術純熟',
+        tags: ['可無套', '可後入', '可乳交', '經驗豐富']
+    },
+    {
+        id: 9,
+        images: ['static/images/girls/a10.jpg', 'static/images/girls/a13.jpg', 'static/images/girls/a1.jpg'],
+        name: '娜娜',
+        age: 25,
+        ageGroup: '18-25',
+        style: 'pure',
+        body: 'slim',
+        description: '氣質空姐，優雅外表下藏著慾火',
+        tags: ['可口交', '可內射', '可雙飛', '配合度高']
+    },
+    {
+        id: 10,
+        images: ['static/images/girls/a11.jpg', 'static/images/girls/a14.jpg', 'static/images/girls/a2.jpg'],
+        name: '思思',
+        age: 22,
+        ageGroup: '18-25',
+        style: 'sweet',
+        body: 'fit',
+        description: '甜美主播，健美身材喜歡角色扮演',
+        tags: ['可顏射', '可口交', '可雙飛', '會玩角色']
+    },
+    {
+        id: 11,
+        images: ['static/images/girls/a12.jpg', 'static/images/girls/a15.jpg', 'static/images/girls/a3.jpg'],
+        name: '珊珊',
+        age: 27,
+        ageGroup: '26-35',
+        style: 'sexy',
+        body: 'fit',
+        description: '性感模特，身材極品技術一流',
+        tags: ['可乳交', '可無套', '可後入', '可內射']
+    },
+    {
+        id: 12,
+        images: ['static/images/girls/a4.jpg', 'static/images/girls/a7.jpg', 'static/images/girls/a10.jpg'],
+        name: '萱萱',
+        age: 24,
+        ageGroup: '18-25',
+        style: 'pure',
+        body: 'fit',
+        description: '瑜伽老師，柔軟度極佳',
+        tags: ['可口交', '可內射', '體位多', '柔軟度好']
+    },
+    {
+        id: 13,
+        images: ['static/images/girls/a5.jpg', 'static/images/girls/a8.jpg', 'static/images/girls/a11.jpg'],
+        name: '如如',
+        age: 30,
+        ageGroup: '26-35',
+        style: 'mature',
+        body: 'slim',
+        description: '苗條熟女，飢渴難耐求滿足',
+        tags: ['可無套', '可雙飛', '可後入', '超級飢渴']
+    },
+    {
+        id: 14,
+        images: ['static/images/girls/a6.jpg', 'static/images/girls/a9.jpg', 'static/images/girls/a12.jpg'],
+        name: '可可',
+        age: 20,
+        ageGroup: '18-25',
+        style: 'sexy',
+        body: 'slim',
+        description: '性感舞者，身材窈窕會跳艷舞',
+        tags: ['可口交', '可乳交', '可顏射', '會跳艷舞']
+    },
+    {
+        id: 15,
+        images: ['static/images/girls/a13.jpg', 'static/images/girls/a14.jpg', 'static/images/girls/a15.jpg'],
+        name: '芳芳',
+        age: 29,
+        ageGroup: '26-35',
+        style: 'sexy',
+        body: 'curvy',
+        description: '成熟老闆娘，風情萬種技術精湛',
+        tags: ['可無套', '可內射', '可後入', '經驗超豐富']
+    },
+    {
+        id: 16,
+        images: ['static/images/girls/a1.jpg', 'static/images/girls/a5.jpg', 'static/images/girls/a9.jpg'],
+        name: '麗麗',
+        age: 18,
+        ageGroup: '18-25',
+        style: 'pure',
+        body: 'curvy',
+        description: '清純大學生，外表文靜內心火熱',
+        tags: ['可內射', '可乳交', '可雙飛', '超乖巧']
+    },
+    {
+        id: 17,
+        images: ['static/images/girls/a2.jpg', 'static/images/girls/a6.jpg', 'static/images/girls/a10.jpg'],
+        name: '晴晴',
+        age: 26,
+        ageGroup: '26-35',
+        style: 'pure',
+        body: 'curvy',
+        description: '優雅教師，豐滿身材氣質佳',
+        tags: ['可口交', '可後入', '可乳交', '技術好']
+    },
+    {
+        id: 18,
+        images: ['static/images/girls/a3.jpg', 'static/images/girls/a7.jpg', 'static/images/girls/a11.jpg'],
+        name: '夢夢',
+        age: 31,
+        ageGroup: '26-35',
+        style: 'sexy',
+        body: 'slim',
+        description: '性感熟女，苗條身材風韻猶存',
+        tags: ['可無套', '可雙飛', '可顏射', '超會玩']
+    },
+    {
+        id: 19,
+        images: ['static/images/girls/a4.jpg', 'static/images/girls/a8.jpg', 'static/images/girls/a12.jpg'],
+        name: '柔柔',
+        age: 33,
+        ageGroup: '26-35',
+        style: 'mature',
+        body: 'fit',
+        description: '成熟健美，保養得宜身材緊實',
+        tags: ['可後入', '可內射', '可口交', '持久力強']
+    },
+    {
+        id: 20,
+        images: ['static/images/girls/a5.jpg', 'static/images/girls/a9.jpg', 'static/images/girls/a13.jpg'],
+        name: '悅悅',
+        age: 22,
+        ageGroup: '18-25',
+        style: 'pure',
+        body: 'fit',
+        description: '清純運動妹，健美身材活力十足',
+        tags: ['可口交', '可內射', '體位多', '超有活力']
+    },
+    {
+        id: 21,
+        images: ['static/images/girls/a6.jpg', 'static/images/girls/a10.jpg', 'static/images/girls/a14.jpg'],
+        name: '甜甜',
+        age: 27,
+        ageGroup: '26-35',
+        style: 'sweet',
+        body: 'slim',
+        description: '甜美OL，苗條可愛討人喜歡',
+        tags: ['可雙飛', '可內射', '可顏射', '超甜美']
+    },
+    {
+        id: 22,
+        images: ['static/images/girls/a7.jpg', 'static/images/girls/a11.jpg', 'static/images/girls/a15.jpg'],
+        name: '蜜蜜',
+        age: 26,
+        ageGroup: '26-35',
+        style: 'sweet',
+        body: 'curvy',
+        description: '甜美人妻，豐滿性感超好相處',
+        tags: ['可乳交', '可無套', '可後入', '溫柔體貼']
+    },
+    {
+        id: 23,
+        images: ['static/images/girls/a8.jpg', 'static/images/girls/a12.jpg', 'static/images/girls/a1.jpg'],
+        name: '嬌嬌',
+        age: 28,
+        ageGroup: '26-35',
+        style: 'pure',
+        body: 'slim',
+        description: '清純熟女，苗條優雅氣質好',
+        tags: ['可口交', '可內射', '配合度高', '超優雅']
+    },
+    {
+        id: 24,
+        images: ['static/images/girls/a9.jpg', 'static/images/girls/a13.jpg', 'static/images/girls/a2.jpg'],
+        name: '媚媚',
+        age: 30,
+        ageGroup: '26-35',
+        style: 'pure',
+        body: 'curvy',
+        description: '優雅人妻，豐滿身材氣質佳',
+        tags: ['可乳交', '可雙飛', '可後入', '經驗豐富']
+    },
+    {
+        id: 25,
+        images: ['static/images/girls/a10.jpg', 'static/images/girls/a14.jpg', 'static/images/girls/a3.jpg'],
+        name: '妮妮',
+        age: 25,
+        ageGroup: '18-25',
+        style: 'sweet',
+        body: 'fit',
+        description: '甜美健身教練，身材緊實可愛',
+        tags: ['可口交', '可內射', '體位多', '超健康']
+    },
+    {
+        id: 26,
+        images: ['static/images/girls/a11.jpg', 'static/images/girls/a15.jpg', 'static/images/girls/a4.jpg'],
+        name: '依依',
+        age: 32,
+        ageGroup: '26-35',
+        style: 'pure',
+        body: 'fit',
+        description: '清純熟女，健美身材保養極佳',
+        tags: ['可雙飛', '可無套', '可後入', '超會保養']
+    },
+    {
+        id: 27,
+        images: ['static/images/girls/a12.jpg', 'static/images/girls/a1.jpg', 'static/images/girls/a5.jpg'],
+        name: '靜靜',
+        age: 29,
+        ageGroup: '26-35',
+        style: 'sweet',
+        body: 'slim',
+        description: '甜美熟女，苗條身材溫柔可人',
+        tags: ['可口交', '可內射', '可顏射', '超溫柔']
+    },
+    {
+        id: 28,
+        images: ['static/images/girls/a13.jpg', 'static/images/girls/a2.jpg', 'static/images/girls/a6.jpg'],
+        name: '芯芯',
+        age: 28,
+        ageGroup: '26-35',
+        style: 'sweet',
+        body: 'curvy',
+        description: '甜美人妻，豐滿性感溫柔體貼',
+        tags: ['可乳交', '可雙飛', '可無套', '超貼心']
+    },
+    {
+        id: 29,
+        images: ['static/images/girls/a14.jpg', 'static/images/girls/a3.jpg', 'static/images/girls/a7.jpg'],
+        name: '薇薇',
+        age: 31,
+        ageGroup: '26-35',
+        style: 'sweet',
+        body: 'fit',
+        description: '甜美熟女，健美身材活力充沛',
+        tags: ['可後入', '可內射', '體位多', '超有活力']
+    },
+    {
+        id: 30,
+        images: ['static/images/girls/a15.jpg', 'static/images/girls/a4.jpg', 'static/images/girls/a8.jpg'],
+        name: '冰冰',
+        age: 27,
+        ageGroup: '26-35',
+        style: 'mature',
+        body: 'slim',
+        description: '成熟知性，苗條優雅氣質佳',
+        tags: ['可口交', '可雙飛', '配合度高', '超知性']
+    },
+    {
+        id: 31,
+        images: ['static/images/girls/a1.jpg', 'static/images/girls/a6.jpg', 'static/images/girls/a11.jpg'],
+        name: '雅雅',
+        age: 35,
+        ageGroup: '35+',
+        style: 'mature',
+        body: 'curvy',
+        description: '成熟女神，豐滿性感經驗豐富',
+        tags: ['可無套', '可雙飛', '可乳交', '技術精湛']
+    },
+    {
+        id: 32,
+        images: ['static/images/girls/a2.jpg', 'static/images/girls/a7.jpg', 'static/images/girls/a12.jpg'],
+        name: '慧慧',
+        age: 36,
+        ageGroup: '35+',
+        style: 'sexy',
+        body: 'fit',
+        description: '性感熟女，健美身材風韻猶存',
+        tags: ['可後入', '可內射', '體位多', '超會玩']
+    },
+    {
+        id: 33,
+        images: ['static/images/girls/a3.jpg', 'static/images/girls/a8.jpg', 'static/images/girls/a13.jpg'],
+        name: '芸芸',
+        age: 37,
+        ageGroup: '35+',
+        style: 'pure',
+        body: 'slim',
+        description: '清純熟女，苗條優雅氣質佳',
+        tags: ['可口交', '可雙飛', '配合度高', '超溫柔']
+    },
+    {
+        id: 34,
+        images: ['static/images/girls/a4.jpg', 'static/images/girls/a9.jpg', 'static/images/girls/a14.jpg'],
+        name: '玲玲',
+        age: 38,
+        ageGroup: '35+',
+        style: 'sweet',
+        body: 'curvy',
+        description: '甜美人妻，豐滿性感溫柔體貼',
+        tags: ['可乳交', '可無套', '可後入', '超貼心']
+    },
+    {
+        id: 35,
+        images: ['static/images/girls/a5.jpg', 'static/images/girls/a10.jpg', 'static/images/girls/a15.jpg'],
+        name: '瑤瑤',
+        age: 39,
+        ageGroup: '35+',
+        style: 'mature',
+        body: 'fit',
+        description: '成熟知性，健美身材保養極佳',
+        tags: ['可內射', '可雙飛', '體位多', '經驗豐富']
+    }
+];
+
+// 当前筛选条件
+let currentFilters = {
+    age: 'all',
+    style: 'all',
+    body: 'all'
+};
+
+let filteredGirls = [];
+
+// 初始化
+document.addEventListener('DOMContentLoaded', function() {
+    initFilters();
+    renderGirls();
+});
+
+// 显示弹窗
+function showModal() {
+    const modal = document.getElementById('successModal');
+    modal.classList.add('active');
+    document.body.style.overflow = 'hidden';
+}
+
+// 关闭弹窗
+function closeModal() {
+    const modal = document.getElementById('successModal');
+    modal.classList.remove('active');
+    document.body.style.overflow = '';
+}
+
+// 初始化筛选器
+function initFilters() {
+    const ageFilter = document.getElementById('age-filter');
+    const styleFilter = document.getElementById('style-filter');
+    const bodyFilter = document.getElementById('body-filter');
+
+    ageFilter.addEventListener('click', (e) => {
+        if (e.target.classList.contains('filter-btn')) {
+            setActiveFilter(ageFilter, e.target);
+            currentFilters.age = e.target.dataset.filter;
+            renderGirls();
+        }
+    });
+
+    styleFilter.addEventListener('click', (e) => {
+        if (e.target.classList.contains('filter-btn')) {
+            setActiveFilter(styleFilter, e.target);
+            currentFilters.style = e.target.dataset.filter;
+            renderGirls();
+        }
+    });
+
+    bodyFilter.addEventListener('click', (e) => {
+        if (e.target.classList.contains('filter-btn')) {
+            setActiveFilter(bodyFilter, e.target);
+            currentFilters.body = e.target.dataset.filter;
+            renderGirls();
+        }
+    });
+}
+
+// 设置活动筛选按钮
+function setActiveFilter(container, activeBtn) {
+    container.querySelectorAll('.filter-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    activeBtn.classList.add('active');
+}
+
+// 渲染女性卡片
+function renderGirls() {
+    const grid = document.getElementById('girls-grid');
+    
+    // 筛选数据
+    filteredGirls = girlsData.filter(girl => {
+        const ageMatch = currentFilters.age === 'all' || girl.ageGroup === currentFilters.age;
+        const styleMatch = currentFilters.style === 'all' || girl.style === currentFilters.style;
+        const bodyMatch = currentFilters.body === 'all' || girl.body === currentFilters.body;
+        return ageMatch && styleMatch && bodyMatch;
+    });
+
+    // 生成HTML
+    if (filteredGirls.length === 0) {
+        grid.innerHTML = '<div style="grid-column: 1/-1; text-align: center; padding: 40px; color: #fff; font-size: 18px;">暫無符合條件的會員，請調整篩選條件</div>';
+        return;
+    }
+
+    grid.innerHTML = filteredGirls.map((girl, index) => `
+        <div class="girl-card">
+            <div class="girl-image-container">
+                <img src="${girl.images[0]}" alt="${girl.name}" class="girl-image">
+                <div class="online-badge">在線中</div>
+            </div>
+            <div class="girl-info">
+                <div class="girl-header">
+                    <div class="girl-name">${girl.name}</div>
+                    <div class="girl-age">${girl.age}歲</div>
+                </div>
+                <div class="girl-desc">${girl.description}</div>
+                <div class="girl-tags">
+                    ${girl.tags.map(tag => `<span class="tag ${tag.includes('雙飛') || tag.includes('無套') || tag.includes('內射') ? 'hot' : ''}">${tag}</span>`).join('')}
+                </div>
+            </div>
+        </div>
+    `).join('');
+}
+
+
+// 平滑滚动
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const href = this.getAttribute('href');
+        if (href === '#' || href.startsWith('#!')) return;
+        
+        e.preventDefault();
+        const target = document.querySelector(href);
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
